2dfe1aaabe3ef700f5b18cada6f4f1bd8a8c1492
xinpengfei520 <542270559@qq.com>
2018-06-10 22:28:39 +0800

    language

--- app/src/main/java/com/xpf/p2p/fragment/MoreFragment.java
+++ app/src/main/java/com/xpf/p2p/fragment/MoreFragment.java
@@ -1,12 +1,15 @@
 package com.xpf.p2p.fragment;
 
+import android.content.Intent;
 import android.widget.TextView;
 
 import com.loopj.android.http.RequestParams;
 import com.xpf.common.base.BaseFragment;
 import com.xpf.p2p.R;
+import com.xpf.p2p.multilanguage.view.MultiLanguageActivity;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by xpf on 2016/11/11 :)
@@ -18,6 +21,8 @@ public class MoreFragment extends BaseFragment {
 
     @BindView(R.id.tv_content)
     TextView tvContent;
+    @BindView(R.id.tvLanguage)
+    TextView tvLanguage;
 
     @Override
     public int getLayoutId() {
@@ -36,12 +41,15 @@ public class MoreFragment extends BaseFragment {
 
     @Override
     protected void initData(String content) {
-
-        // 方式一:
-//        tvContent.setFocusable(true); // 获取焦点
-//        tvContent.setFocusableInTouchMode(true);
-//        tvContent.requestFocus();
-        // 方式二:提供TextView的子视图并重写isFocus()方法
+        /*// 方式一:
+        tvContent.setFocusable(true); // 获取焦点
+        tvContent.setFocusableInTouchMode(true);
+        tvContent.requestFocus();
+        // 方式二:提供TextView的子视图并重写isFocus()方法*/
     }
 
+    @OnClick(R.id.tvLanguage)
+    public void onViewClicked() {
+        startActivity(new Intent(mContext, MultiLanguageActivity.class));
+    }
 }
