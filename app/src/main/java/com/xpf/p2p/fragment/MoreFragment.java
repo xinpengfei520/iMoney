504ade544e1df6de3f2bd07e5c8f7ec2ec88a0a0
xinpengfei <542270559@qq.com>
2019-11-18 13:56:27 +0800

    add webview page.

--- app/src/main/java/com/xpf/p2p/fragment/MoreFragment.java
+++ app/src/main/java/com/xpf/p2p/fragment/MoreFragment.java
@@ -1,6 +1,14 @@
 package com.xpf.p2p.fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Build;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.loopj.android.http.RequestParams;
@@ -23,6 +31,11 @@ public class MoreFragment extends BaseFragment {
     TextView tvContent;
     @BindView(R.id.tvLanguage)
     TextView tvLanguage;
+    @BindView(R.id.webView)
+    WebView webView;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    private static final String TAG = "MoreFragment";
 
     @Override
     public int getLayoutId() {
@@ -46,6 +59,40 @@ public class MoreFragment extends BaseFragment {
         tvContent.setFocusableInTouchMode(true);
         tvContent.requestFocus();
         // 方式二:提供TextView的子视图并重写isFocus()方法*/
+        initData();
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void initData() {
+        webView.getSettings().setJavaScriptEnabled(true);
+        // 设置缓存模式
+        webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        // 开启 DOM storage API 功能
+        webView.getSettings().setDomStorageEnabled(true);
+        // 解除数据阻止
+        webView.getSettings().setBlockNetworkImage(false);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
+        }
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progressBar.setProgress(newProgress);
+                if (newProgress == 100) {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        webView.loadUrl("https://github.com/xinpengfei520/iMoney");
     }
 
     @OnClick(R.id.tvLanguage)
